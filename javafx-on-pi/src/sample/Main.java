dd7ee5896625f38c6f9ec81903722d9f83cf36ba
WilliamBergamin <berw@videotron.ca>
2020-01-20 20:06:30 -0500

    New javafx for machine

---
+++ javafx-on-pi/src/sample/Main.java
@@ -0,0 +1,57 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+//        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        TextField textField = new TextField();
+
+        Button btn = new Button();
+        btn.setText("enter");
+        btn.setStyle("-fx-background-color: #7e7e7e;\n" +
+                "    -fx-background-radius: 30;\n" +
+                "    -fx-background-insets: 0;\n" +
+                "    -fx-text-fill: white;");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+               System.out.println(textField.getText());
+            }
+        });
+
+        GridPane grid = new GridPane();
+        grid.setVgap(3);
+        grid.setHgap(3);
+        grid.setPadding(new Insets(5, 5, 5, 5));
+        grid.add(new Label("To: "), 0, 0);
+        grid.add(textField, 1, 0);
+        grid.add(btn, 1, 1);
+
+        primaryStage.setTitle("hola");
+        StackPane root = new StackPane();
+        root.getChildren().add(grid);
+        primaryStage.setScene(new Scene(root, 900, 400));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
