8ecfc5ad2cb7206e76bc863987f9f478b610289f
WilliamBergamin <Berw@videotron.ca>
2020-02-25 17:51:39 -0500

    update

--- Admin_App/Auth.js
+++ Admin_App/Auth.js
@@ -1,73 +1,70 @@
-import Router from 'next/router'
-import fetch from 'isomorphic-unfetch';
-import Cookies from 'universal-cookie';
+import Router from "next/router";
+import fetch from "isomorphic-unfetch";
+import Cookies from "universal-cookie";
 
 const baseURL = "http://3.133.81.46:80";
 
 class Auth {
+  constructor() {
+    this.cookies = null;
+    this.idToken = null;
+    this.expiresAt = null;
+  }
 
-    constructor() {
-        this.cookies = null;
-        this.idToken = null;
-        this.expiresAt = null;
-    }
+  initialise(req) {
+    this.cookies = new Cookies(req.cookies);
+    this.idToken = this.cookies.get("token");
+    this.expiresAt = this.cookies.get("expiresAt");
+  }
 
-    initialise(req) {
-        this.cookies = new Cookies(req.cookies);
-        this.idToken = this.cookies.get('token');
-        this.expiresAt = this.cookies.get('expiresAt');
-    }
+  login() {
+    Router.push("/");
+  }
 
-    login() {
-        Router.push('/')
-    }
+  logout() {
+    // TODO add endpoint to void token
+    this.idToken = null;
+    this.expiresAt = 0;
+    this.cookies.set("token", this.idToken);
+    this.cookies.set("expiresAt", this.expiresAt);
+  }
 
-    logout() {
-        // TODO add endpoint to void token
-        this.idToken = null;
-        this.expiresAt = 0;
-        this.cookies.set('token', this.idToken)
-        this.cookies.set('expiresAt', this.expiresAt)
-    }
+  getIdToken() {
+    return this.idToken;
+  }
 
-    getIdToken() {
-        return this.idToken;
-    }
+  handleAuthentication(email, password) {
+    return new Promise(async (resolve, reject) => {
+      const response = await fetch(baseURL + "/api/v1/user/token", {
+        method: "POST",
+        body: JSON.stringify({
+          email: email,
+          password: password
+        }),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });
+      const responseJson = await response.json();
+      if (!responseJson || !responseJson.token) {
+        return reject();
+      }
+      this.setSession(responseJson);
+      resolve();
+    });
+  }
 
-    handleAuthentication(email, password) {
-        return new Promise(async (resolve, reject) => {
-            const response = await fetch(baseURL + '/api/v1/user/token', {
-                method: 'POST',
-                body: JSON.stringify({
-                    "email": email,
-                    "password": password
-                }),
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            });
-            console.log(response)
-            const responseJson = await response.json();
-            console.log(responseJson);
-            if (!responseJson || !responseJson.token) {
-                return reject();
-            }
-            this.setSession(responseJson);
-            resolve();
-        });
-    }
+  setSession(User) {
+    this.idToken = User.token;
+    this.expiresAt = 86400 * 1000 + new Date().getTime();
+    this.cookies.set("token", this.idToken);
+    this.cookies.set("expiresAt", this.expiresAt);
+  }
 
-    setSession(User) {
-        this.idToken = User.token;
-        this.expiresAt = 86400 * 1000 + new Date().getTime();
-        this.cookies.set('token', this.idToken)
-        this.cookies.set('expiresAt', this.expiresAt)
-    }
-
-    isAuthenticated() {
-        // Check whether the current time is past the token's expiry time
-        return new Date().getTime() < this.expiresAt;
-    }
+  isAuthenticated() {
+    // Check whether the current time is past the token's expiry time
+    return new Date().getTime() < this.expiresAt;
+  }
 }
 
 const auth = new Auth();
--- Admin_App/pages/eventSearch.jsx
+++ Admin_App/pages/eventSearch.jsx
@@ -1,253 +1,120 @@
-import React from 'react';
+import React from "react";
 // eslint-disable-next-line no-unused-vars
-import fetch from 'isomorphic-unfetch';
-import Toolbar from '@material-ui/core/Toolbar';
-import Cookies from 'universal-cookie';
-import { Grid } from '@material-ui/core';
-import ToolBar from '../components/ToolBar';
-import auth from '../Auth';
-import CircularIndeterminate from '../components/CircularIndeterminate';
-import YouMustLogIn from '../components/YouMustLogIn'
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import DraftsIcon from '@material-ui/icons/Drafts';
-import useListStyle from '../styles/eventSearchStyle';
-import useIndexStyle from '../styles/IndexStyle';
-import Paper from '@material-ui/core/Paper';
-import TextField from '@material-ui/core/TextField';
-import SearchIcon from '@material-ui/icons/Search';
-import IconButton from '@material-ui/core/IconButton';
+import fetch from "isomorphic-unfetch";
+import Cookies from "universal-cookie";
+import { Grid } from "@material-ui/core";
+import ToolBar from "../components/ToolBar";
+import auth from "../Auth";
+import YouMustLogIn from "../components/YouMustLogIn";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import EventIcon from "@material-ui/icons/Event";
+import useListStyle from "../styles/eventSearchStyle";
+import useIndexStyle from "../styles/IndexStyle";
+import Paper from "@material-ui/core/Paper";
+import TextField from "@material-ui/core/TextField";
+import SearchIcon from "@material-ui/icons/Search";
+import IconButton from "@material-ui/core/IconButton";
 
+const baseURL = "http://3.133.81.46:80";
 
 function ListItemLink(props) {
-    return <ListItem button component="a" {...props} />;
+  return <ListItem button component="a" {...props} />;
 }
 
-export default function ContainerFeed(req) {
-    const cookies = new Cookies(req.cookies);
-    auth.initialise(req);
+class ContainerFeed extends React.Component {
+  constructor(props) {
+    super(props);
+    auth.initialise(props);
+    //   const cookies = new Cookies(req.cookies);
+  }
 
+  state = {
+    allEvents: [],
+    events: []
+  };
+
+  componentDidMount() {
+    fetch(baseURL + "/api/v1/events", {
+      method: "GET",
+      headers: {
+        Authorization: "Token " + auth.getIdToken()
+      }
+    })
+      .then(res => res.json())
+      .then(json => this.setState({ allEvents: json, events: json }));
+  }
+
+  render() {
+    const { events } = this.state;
     return !auth.isAuthenticated() ? (
-        <YouMustLogIn />
+      <YouMustLogIn />
     ) : (
-            <Grid container justify="center" style={{ width: '100%', marginTop: '5rem' }}>
-                <Paper style={useListStyle.paper}>
-                    <ToolBar />
-                    <List style={{ width: '100%' }}
-                        subheader={
-                            <Grid container justify="center" direction="row" alignItems="center" style={{ width: '100%' }}>
-                                <TextField
-                                    style={{ marginLeft: '1%', marginRight: '1%', marginBottom: '1%' }}
-                                    className={useIndexStyle.textField}
-                                    InputProps={{
-                                        className: useIndexStyle.input,
-                                    }}
-                                    label="Search"
-                                    name="Search"
-                                    variant="outlined"
-                                    onChange={(event) => {
-                                        // setTempNumberOrder(parseInt(event.target.value.replace(/[^0-9]+/g, ''), 10));
-                                    }}
-                                    onKeyDown={(e) => {
-                                        if (e.keyCode === 13) {
-                                            // setNumberOrder(tempNumberOrder);
-                                        }
-                                    }}
-                                />
-                                <IconButton
-                                    style={{ marginBottom: '1%' }}
-                                    onClick={() => {
-                                        // setNumberOrder(tempNumberOrder); 
-                                    }}>
-                                    <SearchIcon />
-                                </IconButton>
-                            </Grid>
-                        }>
-                        <ListItem button>
-                            <ListItemIcon>
-                                <InboxIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Inbox" />
-                        </ListItem>
-                        <ListItem button>
-                            <ListItemIcon>
-                                <DraftsIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Drafts" />
-                        </ListItem>
-                        <ListItem button>
-                            <ListItemIcon>
-                                <InboxIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Inbox" />
-                        </ListItem>
-                        <ListItem button>
-                            <ListItemIcon>
-                                <DraftsIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Drafts" />
-                        </ListItem>
-                        <ListItem button>
-                            <ListItemIcon>
-                                <InboxIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Inbox" />
-                        </ListItem>
-                        <ListItem button>
-                            <ListItemIcon>
-                                <DraftsIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Drafts" />
-                        </ListItem>
-                        <ListItem button>
-                            <ListItemIcon>
-                                <InboxIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Inbox" />
-                        </ListItem>
-                        <ListItem button>
-                            <ListItemIcon>
-                                <DraftsIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Drafts" />
-                        </ListItem>
-                        <ListItem button>
-                            <ListItemIcon>
-                                <InboxIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Inbox" />
-                        </ListItem>
-                        <ListItem button>
-                            <ListItemIcon>
-                                <DraftsIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Drafts" />
-                        </ListItem>
-                        <ListItem button>
-                            <ListItemIcon>
-                                <InboxIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Inbox" />
-                        </ListItem>
-                        <ListItem button>
-                            <ListItemIcon>
-                                <DraftsIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Drafts" />
-                        </ListItem>
-                        <ListItem button>
-                            <ListItemIcon>
-                                <InboxIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Inbox" />
-                        </ListItem>
-                        <ListItem button>
-                            <ListItemIcon>
-                                <DraftsIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Drafts" />
-                        </ListItem>
-                        <ListItem button>
-                            <ListItemIcon>
-                                <InboxIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Inbox" />
-                        </ListItem>
-                        <ListItem button>
-                            <ListItemIcon>
-                                <DraftsIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Drafts" />
-                        </ListItem>
-                        <ListItem button>
-                            <ListItemIcon>
-                                <InboxIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Inbox" />
-                        </ListItem>
-                        <ListItem button>
-                            <ListItemIcon>
-                                <DraftsIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Drafts" />
-                        </ListItem>
-                        <ListItem button>
-                            <ListItemIcon>
-                                <InboxIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Inbox" />
-                        </ListItem>
-                        <ListItem button>
-                            <ListItemIcon>
-                                <DraftsIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Drafts" />
-                        </ListItem>
-                        <ListItem button>
-                            <ListItemIcon>
-                                <InboxIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Inbox" />
-                        </ListItem>
-                        <ListItem button>
-                            <ListItemIcon>
-                                <DraftsIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Drafts" />
-                        </ListItem>
-                        <ListItem button>
-                            <ListItemIcon>
-                                <InboxIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Inbox" />
-                        </ListItem>
-                        <ListItem button>
-                            <ListItemIcon>
-                                <DraftsIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Drafts" />
-                        </ListItem>
-                        <ListItem button>
-                            <ListItemIcon>
-                                <InboxIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Inbox" />
-                        </ListItem>
-                        <ListItem button>
-                            <ListItemIcon>
-                                <DraftsIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Drafts" />
-                        </ListItem>
-                        <ListItem button>
-                            <ListItemIcon>
-                                <InboxIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Inbox" />
-                        </ListItem>
-                        <ListItem button>
-                            <ListItemIcon>
-                                <DraftsIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Drafts" />
-                        </ListItem>
-                        <ListItem button>
-                            <ListItemIcon>
-                                <InboxIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Inbox" />
-                        </ListItem>
-                        <ListItem button>
-                            <ListItemIcon>
-                                <DraftsIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Drafts" />
-                        </ListItem>
-                    </List>
-                </Paper>
-            </Grid>
-        );
+      <Grid
+        container
+        justify="center"
+        style={{ width: "100%", marginTop: "5rem" }}
+      >
+        <Paper style={useListStyle.paper}>
+          <ToolBar />
+          <List
+            style={{ width: "100%" }}
+            subheader={
+              <Grid
+                container
+                justify="center"
+                direction="row"
+                alignItems="center"
+                style={{ width: "100%" }}
+              >
+                <TextField
+                  style={{
+                    marginLeft: "1%",
+                    marginRight: "1%",
+                    marginBottom: "1%"
+                  }}
+                  className={useIndexStyle.textField}
+                  InputProps={{
+                    className: useIndexStyle.input
+                  }}
+                  label="Search"
+                  name="Search"
+                  variant="outlined"
+                  onChange={event => {
+                    // setTempNumberOrder(parseInt(event.target.value.replace(/[^0-9]+/g, ''), 10));
+                  }}
+                  onKeyDown={e => {
+                    if (e.keyCode === 13) {
+                      // setNumberOrder(tempNumberOrder);
+                    }
+                  }}
+                />
+                <IconButton
+                  style={{ marginBottom: "1%" }}
+                  onClick={() => {
+                    // setNumberOrder(tempNumberOrder);
+                  }}
+                >
+                  <SearchIcon />
+                </IconButton>
+              </Grid>
+            }
+          >
+            {events.map(event => (
+              <ListItem button>
+                <ListItemIcon>
+                  <EventIcon />
+                </ListItemIcon>
+                <ListItemText primary={event.name} secondary={event.location} />
+              </ListItem>
+            ))}
+          </List>
+        </Paper>
+      </Grid>
+    );
+  }
 }
+
+export default ContainerFeed;
