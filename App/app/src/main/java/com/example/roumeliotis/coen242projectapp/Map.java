81ee08d5c8474d5b735de4f08eb0fd3820e1f5ce
croumeliotis <dina2395@hotmail.com>
2020-03-04 14:56:55 -0500

    Google Maps API added
    No display on emulator, no markers added yet

--- App/app/src/main/java/com/example/roumeliotis/coen242projectapp/Map.java
+++ App/app/src/main/java/com/example/roumeliotis/coen242projectapp/Map.java
@@ -3,10 +3,15 @@ package com.example.roumeliotis.coen242projectapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.ImageButton;
 
-public class Map extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class Map extends AppCompatActivity implements OnMapReadyCallback{
 
     ServerHelper serverHelper;
     Manager Manager;
@@ -14,27 +19,35 @@ public class Map extends AppCompatActivity {
     String eventKey;
     User user;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map_page);
 
-        getSupportActionBar().setTitle(R.string.app_name);
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
         serverHelper = new ServerHelper();
         Manager = new Manager(this);
         Intent userInfo = getIntent();
         user = userInfo.getParcelableExtra("user");
         eventKey = userInfo.getStringExtra("eventKey");
         backToOrder = findViewById(R.id.returnButton);
-
+/*
         backToOrder.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 goToCreateDrink();
             }
-        });
+        });*/
     }
 
+    @Override
+    public void onMapReady(GoogleMap map) {
+        map.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+    }
 
     void goToCreateDrink() {
         Intent intent = new Intent();
