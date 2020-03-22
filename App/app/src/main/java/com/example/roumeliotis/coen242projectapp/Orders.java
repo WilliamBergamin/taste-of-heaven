97d9b115e25e5e0578db12c2b123084acf44ac8a
croumeliotis <dina2395@hotmail.com>
2020-03-22 01:54:09 -0400

    QR Codes for drinks (in progress)
    Can generate and display QR code. Need to make ListView or something to display each QR code with its appropriate order)

--- App/app/src/main/java/com/example/roumeliotis/coen242projectapp/Orders.java
+++ App/app/src/main/java/com/example/roumeliotis/coen242projectapp/Orders.java
@@ -1,16 +1,21 @@
 package com.example.roumeliotis.coen242projectapp;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
 
 // View all of your orders to gain access to input code
 public class Orders extends AppCompatActivity{
@@ -20,6 +25,12 @@ public class Orders extends AppCompatActivity{
     Manager Manager;
     String eventKey;
     User user;
+
+    // QR Stuff
+    Bitmap bitmap;
+    ImageView qrImage;
+    ListView qrList;
+    QRGEncoder qrgEncoder; // = new QRGEncoder(inputValue, null, QRGContents.Type.TEXT, smallerDimension);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,14 +43,27 @@ public class Orders extends AppCompatActivity{
         Intent userInfo = getIntent();
         user = userInfo.getParcelableExtra("user");
         eventKey = userInfo.getStringExtra("eventKey");
+        qrImage = (ImageView) findViewById(R.id.QR_Image);
 
         ordersList = findViewById(R.id.orderCodesList);
 
         List<Order> orders = Manager.getOrdersByUserID(user.getid());
         List<String> orderStrings = new ArrayList<String>();
+        List<ImageView> qrImages = new ArrayList<ImageView>();
         for (int i = 0; i < orders.size(); i++) {
             Log.d("Orders", orders.get(i).toString());
             orderStrings.add(orders.get(i).toString());
+            qrgEncoder = new QRGEncoder(orders.get(i).getOrder_key(), null, QRGContents.Type.TEXT, 325);
+            try {
+                // Getting QR-Code as Bitmap
+                bitmap = qrgEncoder.encodeAsBitmap();
+                // Setting Bitmap to ImageView
+                qrImage.setImageBitmap(bitmap);
+                qrImages.add(qrImage);
+            }
+            catch(Exception e) {
+                //Log.v(e, e.toString());
+            }
         }
 
         ordersList.setAdapter(new ArrayAdapter<String>(this, R.layout.row, orderStrings));
