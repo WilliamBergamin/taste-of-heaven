4a734462181c5f4bceccd67472c7a355608e47b6
WilliamBergamin <Berw@videotron.ca>
2020-04-13 16:26:47 -0400

    Update MachineMicrocontrolerHelper.java

--- javafx-on-pi/src/sample/MachineMicrocontrolerHelper.java
+++ javafx-on-pi/src/sample/MachineMicrocontrolerHelper.java
@@ -46,6 +46,7 @@ public class MachineMicrocontrolerHelper {
                     System.out.println("[HEX DATA]   " + event.getHexByteString());
                     byte[] received = event.getByteBuffer().array();
                     MachineMicrocontrolerHelper.microState = possibleMicroState.getOrDefault(received[2], "error");
+                    System.out.print("state:"+MachineMicrocontrolerHelper.microState);
                     if (MachineMicrocontrolerHelper.microState == "error"){
                         Machine.setState((short) 2);
                         Machine.setError("Problem with micro controller");
